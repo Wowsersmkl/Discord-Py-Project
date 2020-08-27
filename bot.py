b26e190849603e40d938804e1a28711ade6775f4
Smirf123 <smirf368@outlook.com>
2020-08-26 18:33:15 -0700

    The More Memes Commit
    Fixed the Meme API so now we have memes bois, get a meme with rmemes command

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 loading = discord.emoji 
 
-TOKEN = ('Token Here')
+TOKEN = ('Bot Token Here')
 activity = discord.Game(name="Smirf Made Me")
 
 
@@ -28,14 +28,15 @@ async def on_message(message):
     if message.content.startswith('Im'):
        await message.channel.send("Hi Im dad")
     if message.content.startswith('!help'):
-       await message.channel.send('I am Smirf123#5911 creation, I am a work in progress, I can only do Ping, and Help atm but will be updated in the future')
+       await message.channel.send('I am Smirf123#5911 creation, I am a work in progress, I can only do ping, help, meme, and rmeme Caution the rmeme is a random reddit meme and idk what it will send so it is suggested to send in nsfw channel just incase.')
     if message.content.startswith('!creator'):
        await message.channel.send('The man who created me is Smirf123#5911, he has made a few other bots but this is his fisrt one based on Python, follow him on twitch https://twitch.tv/smirf123')
     if message.content.startswith('!meme'):
        await message.channel.send('It is wednesday my dudes')
-    if message.content.startswith('!random'):
+    if message.content.startswith('!rmeme'):
        await message.channel.send('Gettin ya a meme :loading:')
-       response = requests.get("https://some-random-api.ml/meme")
-       results = (response[0]['image'])
-       await message.channel.send(results)
+       r = requests.get("https://meme-api.herokuapp.com/gimme")
+       json_response = r.json()
+       image = json_response['postLink']
+       await message.channel.send(image)
 client.run(TOKEN)
